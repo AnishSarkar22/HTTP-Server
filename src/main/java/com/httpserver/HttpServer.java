f596962f4e2e5750f8b14dcf31a911b6ffcc2ad0
Anish Sarkar <104695310+AnishSarkar22@users.noreply.github.com>
2025-08-10 03:08:08 +0530

    Add exec-maven-plugin and implement basic HTTP server functionality

--- src/main/java/com/httpserver/HttpServer.java
+++ src/main/java/com/httpserver/HttpServer.java
@@ -1,6 +1,10 @@
 package com.httpserver;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import com.httpserver.config.Configuration;
 import com.httpserver.config.ConfigurationManager;
@@ -25,5 +29,39 @@ public class HttpServer {
 
         System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using WebRoot: " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+
+            // for using `.accept()`, the server waits to get a connection, if no connection is accepted, then it does not execute further, it stays waiting for the connection
+            Socket socket = serverSocket.accept(); // prompts the socket thats listening to a port to accept any connection that arise
+
+            InputStream inputStream = socket.getInputStream(); // allowing the server to read data sent by the client
+            OutputStream outputStream = socket.getOutputStream(); // allowing the server to send data back to the client
+
+            // TODO: we would read
+
+            String html = "<html><head>Java HTTP Server<title></title></head><body><h1>This page was served using my Java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response = 
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line :  HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + //HEADER
+                    CRLF + 
+                    html + 
+                    CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+            
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 }
